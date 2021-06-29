7c24522c909a5f8504983e692a16695ef5a6e87d
05kim <yhkim123@fastcampus.co.kr>
2021-06-29 12:41:40 +0900

    init

---
+++ content/blog/category1/test1.md
@@ -0,0 +1,8 @@
+---
+title: '양성민 입니다.'
+date: 2020-07-02 16:21:13
+category: 'category1'
+draft: false
+---
+
+양많고 싸지요
---
+++ gatsby-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `양성민의 사이트`,
+  description: `Blog posted about ...`,
+  author: `양성민`,
+  introduction: `양싸입니다. 양많고 쌉니다.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
